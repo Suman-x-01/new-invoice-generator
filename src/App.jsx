ac62e5d74e211a7e364c2ca7f5cfb9c090fdd9a1
Suman <sumannayak192113114@gmail.com>
2026-01-27 17:03:14 +0530

    error correct

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,9 @@ import { useState } from "react";
 // import reactLogo from "./assets/react.svg";
 // import viteLogo from "/vite.svg";
 // import "./App.css";
-import GSTInvoiceGenerator from "./Components/GSTInvoicegenerator";
+// import GSTInvoiceGenerator from "./Components/GSTInvoicegenerator";
+import GSTInvoiceGenerator from "./Components/GSTInvoiceGenerator";
+
 function App() {
   const [count, setCount] = useState(0);
 
